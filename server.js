0957e0ebac7374de8f7b4d2d219e431d98edd490
Himanshu Verma | DevOps | Full Stack Developer <79570418+imhimanshu1-stack@users.noreply.github.com>
2024-09-03 19:15:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
-const express = require('express');
-const path = require('path');
+// Retrieve Job-defined env vars
+const {CLOUD_RUN_TASK_INDEX = 0, CLOUD_RUN_TASK_ATTEMPT = 0} = process.env;
+// Retrieve User-defined env vars
+const {SLEEP_MS, FAIL_RATE} = process.env;
 
-const app = express();
+// Define main script
+const main = async () => {
+  console.log(
+    `Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}...`
+  );
+  // Simulate work
+  if (SLEEP_MS) {
+    await sleep(SLEEP_MS);
+  }
+  // Simulate errors
+  if (FAIL_RATE) {
+    try {
+      randomFailure(FAIL_RATE);
+    } catch (err) {
+      err.message = `Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT} failed.\n\n${err.message}`;
+      throw err;
+    }
+  }
+  console.log(`Completed Task #${CLOUD_RUN_TASK_INDEX}.`);
+};
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+// Wait for a specific amount of time
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+// Throw an error based on fail rate
+const randomFailure = rate => {
+  rate = parseFloat(rate);
+  if (!rate || rate < 0 || rate > 1) {
+    console.warn(
+      `Invalid FAIL_RATE env var value: ${rate}. Must be a float between 0 and 1 inclusive.`
+    );
+    return;
+  }
+
+  const randomFailure = Math.random();
+  if (randomFailure < rate) {
+    throw new Error('Task failed.');
+  }
+};
+
+// Start script
+main().catch(err => {
+  console.error(err);
+  process.exit(1); // Retry Job Task by exiting the process
 });
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
